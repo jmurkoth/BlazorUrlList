710879dfc8485d5c7a2714296e416e710c0d1f05
Murkoth, Jeevan <jeevan.murkoth@intel.com>
2021-07-04 17:23:02 -0700

    added auth update and other misc changes

--- JM.BlzrUrlList/JM.BlzrUrlList.Web/UrlService.cs
+++ JM.BlzrUrlList/JM.BlzrUrlList.Web/UrlService.cs
@@ -19,10 +19,38 @@ namespace JM.BlzrUrlList.Web
            
             this.httpClientFactory = httpClientFactory;
         }
+         public async Task<UrlList> DeleteUrlList(UrlList urlList)
+         {
+            var clnt = string.IsNullOrEmpty(urlList?.UserId)? this.httpClientFactory.CreateClient("webapi") : this.httpClientFactory.CreateClient("securewebapi");
 
+            var result = await clnt.DeleteAsync($"/api/UrlList/{urlList.UrlId}");
+            if (result.IsSuccessStatusCode)
+            {
+                return await result.Content.ReadFromJsonAsync<UrlList>();
+            }
+            else
+            {
+                return null;
+            }
+         }
+
+        public async Task<UrlList> UpdateUrlList(UrlList urlList)
+        {
+            var clnt = string.IsNullOrEmpty(urlList?.UserId)? this.httpClientFactory.CreateClient("webapi") : this.httpClientFactory.CreateClient("securewebapi");
+
+            var result = await clnt.PutAsJsonAsync($"/api/UrlList/", urlList);
+            if (result.IsSuccessStatusCode)
+            {
+                return await result.Content.ReadFromJsonAsync<UrlList>();
+            }
+            else
+            {
+                return null;
+            }
+        }
         public async Task<UrlList> SaveUrlList(UrlList urlList)
         {
-            var clnt = this.httpClientFactory.CreateClient("UrlApi");
+            var clnt = string.IsNullOrEmpty(urlList?.UserId)? this.httpClientFactory.CreateClient("webapi") : this.httpClientFactory.CreateClient("securewebapi");
 
             var result = await clnt.PostAsJsonAsync($"/api/UrlList/", urlList);
             if (result.IsSuccessStatusCode)
@@ -34,22 +62,48 @@ namespace JM.BlzrUrlList.Web
                 return null;
             }
         }
+
+        public async Task<List<UrlList>> GetListsForUser( string userid)
+        {
+            var clnt = this.httpClientFactory.CreateClient("securewebapi");
+           try
+            {
+                var result = await clnt.GetAsync($"api/UrlList/users/{userid}");
+                if (result.IsSuccessStatusCode)
+                {
+                    return await result.Content.ReadFromJsonAsync<List<UrlList>>();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            return null;
+        }
         public async Task<UrlList> GetUrlList(string urlId)
         {
-            var clnt = this.httpClientFactory.CreateClient("UrlApi");
-            var result = await clnt.GetAsync($"api/UrlList/{urlId}");
-            if (result.IsSuccessStatusCode)
+            var clnt = this.httpClientFactory.CreateClient("webapi");
+           try
             {
-                return await result.Content.ReadFromJsonAsync<UrlList>();
+                var result = await clnt.GetAsync($"api/UrlList/{urlId}");
+                if (result.IsSuccessStatusCode)
+                {
+                    return await result.Content.ReadFromJsonAsync<UrlList>();
+                }
             }
-            else
+            catch (Exception ex)
             {
-                return null;
+
+                Console.WriteLine(ex.Message);
             }
+
+            return null;
         }
         public async Task<OpenGraphResult> GetUrlMetaData(string url)
         {
-            var clnt = this.httpClientFactory.CreateClient("UrlApi");
+            var clnt = this.httpClientFactory.CreateClient("webapi");
             
             var result = await clnt.PostAsJsonAsync($"/api/OpenGraph/",new OpenGraphRequest(  url ));
             //return await Task.FromResult<OpenGraphResult>( new OpenGraphResult("www.news.google.com", "Google News",
